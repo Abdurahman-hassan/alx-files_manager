40589334eb9a32ab14cd57449ff2c76d86bfb986
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-08-08 02:06:55 +0300

    fix missing files

--- utils/redis.js
+++ utils/redis.js
@@ -1,36 +1,62 @@
-import { createClient } from 'redis';
+import redis from 'redis';
 import { promisify } from 'util';
 
+/**
+ * Class for performing operations with Redis service
+ */
 class RedisClient {
   constructor() {
-    this.client = createClient();
-    this.alive = true;
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
     this.client.on('error', (error) => {
-      console.log(error);
-      this.alive = false;
+      console.log(`Redis client not connected to the server: ${error.message}`);
     });
-    this.client.get = promisify(this.client.get).bind(this.client);
-    this.client.set = promisify(this.client.set).bind(this.client);
-    this.client.once('ready', () => {
-      this.alive = true;
+
+    this.client.on('connect', () => {
+      // console.log('Redis client connected to the server');
     });
   }
 
+  /**
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
+   */
   isAlive() {
-    return this.alive;
+    return this.client.connected;
   }
 
+  /**
+   * gets value corresponding to key in redis
+   * @key {string} key to search for in redis
+   * @return {string}  value of key
+   */
   async get(key) {
-    return this.client.get(key);
+    const value = await this.getAsync(key);
+    return value;
   }
 
+  /**
+   * Creates a new key in redis with a specific TTL
+   * @key {string} key to be saved in redis
+   * @value {string} value to be asigned to key
+   * @duration {number} TTL of key
+   * @return {undefined}  No return
+   */
   async set(key, value, duration) {
-    this.client.set(key, value, 'EX', duration);
+    this.client.setex(key, duration, value);
   }
 
+  /**
+   * Deletes key in redis service
+   * @key {string} key to be deleted
+   * @return {undefined}  No return
+   */
   async del(key) {
-    return this.client.del(key);
+    this.client.del(key);
   }
 }
 
-export default new RedisClient();
+const redisClient = new RedisClient();
+
+export default redisClient;
